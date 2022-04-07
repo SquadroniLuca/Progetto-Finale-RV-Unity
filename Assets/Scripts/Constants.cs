3803607886ec87c5bcef6705a4e3e82e592c5459
Laura Mazzuca <lauramazzuca@protonmail.com>
2022-04-07 16:01:17 +0200

    adds scripts to let the recyclable objects interact with trashcans

---
+++ Assets/Scripts/Constants.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constants
+{
+    namespace Enums
+    {
+        public enum TrashType
+        {
+            PAPER,
+            PLASTIC,
+            ORGANIC,
+            GLASS,
+            TIN,
+            NONRECYCLABLE
+        }
+    }
+    
+}
---
+++ Assets/Scripts/RecyclableObject.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+class RecyclableObject : MonoBehaviour
+{
+    [SerializeField]
+    private Constants.Enums.TrashType _trashType;
+    public Constants.Enums.TrashType TrashType { get { return _trashType; } private set { _trashType = value; } }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        TrashCan can = other.gameObject.GetComponent<TrashCan>();
+        if (can != null && can.TrashType == this.TrashType)
+        {
+            UnityEngine.Debug.Log("correct!");
+        }
+        else
+        {
+            UnityEngine.Debug.Log("wrooong!");
+        }
+    }
+}
